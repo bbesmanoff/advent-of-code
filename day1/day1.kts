3bff06692f8fa588fbabf7ecf95918ae8b1fcab8
Brian Besmanoff <bbesmanoff@gmail.com>
2020-12-03 00:14:34 -0800

    Day 1 part 2

--- day1/day1.kts
+++ day1/day1.kts
@@ -12,9 +12,27 @@ fun combinations(input: List<Int>): Sequence<Pair<Int, Int>> {
     }
 }
 
+fun combinations3(input: List<Int>): Sequence<Triple<Int, Int, Int>> {
+    val length = input.size;
+
+    return sequence {
+        for (aIdx in 0 until length) {
+            for (bIdx in (aIdx + 1) until (length - 1)) {
+                for (cIdx in (bIdx + 1) until length)
+                    yield(Triple(input[aIdx], input[bIdx], input[cIdx]))
+            }
+        }
+    }
+}
+
 val input = File("input.txt").readLines().map(String::toInt)
 
 val day1 = combinations(input)
         .first { (a, b) -> a + b == 2020 }
         .let { (a, b) -> a * b }
 println(day1)
+
+val day2 = combinations3(input)
+        .first { (a, b, c) -> a + b + c == 2020 }
+        .let { (a, b, c) -> a * b * c }
+println(day2)
